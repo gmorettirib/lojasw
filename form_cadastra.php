cd9d4bad533dd6d0b32c6008d846a983377df309
Guilherme Moretti <160153078+gmorettirib@users.noreply.github.com>
2025-08-20 12:25:55 -0300

    Add files via upload

---
+++ form_cadastra.php
@@ -0,0 +1,27 @@
+<?php
+include "pedaco.php"
+?>
+
+
+
+<div class="container">
+    <form>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="nome" placeholder="Digite o nome do produto">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="preco" placeholder="Digite o preço do produto">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="quantidade" placeholder="Digite a quantidade do produto">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ pedaco.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOJINHA ETECMCM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>SISTEMA CRUD - LOJA</h1>
+        <H2> Guilherme Moretti Ribeiro</H2>
+    </div>
